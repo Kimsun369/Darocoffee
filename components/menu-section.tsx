12a7356534c458d75617618d5a2f47c5f0bf11a1
Kimsun369 <143208905+Kimsun369@users.noreply.github.com>
2025-09-01 13:38:06 +0700

    improve design cart menu order to mobile devices

--- components/menu-section.tsx
+++ components/menu-section.tsx
@@ -167,7 +167,7 @@ export function MenuSection({ products, onProductClick, language }: MenuSectionP
                 </div>
 
                 {/* Products grid */}
-                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+                <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 ">
                   {displayProducts.map((product) => (
                     <Card
                       key={product.id}
